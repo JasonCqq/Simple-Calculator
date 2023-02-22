a43c776f28c564c98533d9c32bd32d038d6ad577
Jason <121495300+jason21715@users.noreply.github.com>
2023-02-22 01:40:46 -0800

    Reworked Calculator Functionality

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
+const numButton = document.querySelectorAll(".numberButton");
+const operatorButton = document.querySelectorAll(".normalOperator");
+const equalButton = document.getElementById("equalOperator");
+const clearButton = document.getElementById("clearOperator");
+const mainDisplay = document.getElementById("calculatorDisplay");
+mainDisplay.style.fontSize = "5vh";
+
 const add = (...args) => {
     total = args.reduce((accumul, argument) => {
         return accumul + argument;
     }, 0);
     mainDisplay.value = total;
 }
-
 const subtract = (a, ...args) => {
     total = args.reduce((accumul, argument) => {
         return accumul - argument;
     }, a);
     mainDisplay.value = total;
 }
-
 const multiply = (...args) => {
     total = args.reduce((accumul, argument) => {
         return accumul * argument;
     }, 1);
     mainDisplay.value = total;
 }
-
 const divide = (a, ...args) => {
-    if (args == 0){
+    if (args === 0){
         mainDisplay.value = "ERROR";
+    } else {
+        total = args.reduce((accumul, argument) => {
+            return accumul / argument;
+        }, a);
+        mainDisplay.value = total;
     }
-
-    total = args.reduce((accumul, argument) => {
-        return accumul / argument;
-    }, a);
-    mainDisplay.value = total;
 }
 
 const operate = (operator, a, b) => {
@@ -38,7 +42,7 @@ const operate = (operator, a, b) => {
         case "-":
             subtract(a, b);
             break;
-        case "*":
+        case "×":
             multiply(a, b);
             break;
         case "÷":
@@ -47,51 +51,41 @@ const operate = (operator, a, b) => {
     }
 }
 
-const mainDisplay = document.getElementById("calculatorDisplay");
-mainDisplay.style.fontSize = "5vh";
-
-const numButton = document.querySelectorAll(".numberButton");
-const operatorButton = document.querySelectorAll(".normalOperator");
-const equalsButton = document.getElementById("equalOperator");
-const clearButton = document.getElementById("clearOperator");
-
-let currentOperator;
-let value1;
-let value2;
-
 const mainCalculatorFunction = () => {
+    let operatorCount = 0; // to make sure we get a value2. 
+    let currentOperator;
+    let value1;
+    let value2;
 
     for (const btns of numButton) {
         btns.addEventListener("click", () => {
             mainDisplay.value += btns.innerText;
-        }, );
+        },);
     }
 
     for (const btns of operatorButton) {
         btns.addEventListener("click", () => {
-            if (value1 === undefined) {
+            if (mainDisplay.value !== undefined && operatorCount !== 1){
                 value1 = parseInt(mainDisplay.value);
-                mainDisplay.value = "";
                 console.log(value1);
                 currentOperator = btns.innerText;
+                mainDisplay.value += currentOperator;
+                operatorCount++;
             }
-                
-            if (value1 !== undefined && mainDisplay.value !== undefined) {
-                value2 = parseInt(mainDisplay.value);
-                console.log(value2);
-            }
-        }, {once:true});
+        },);
     }
 
-    equalsButton.addEventListener("click", () => {
-        operate(currentOperator, value1, value2);
+    equalButton.addEventListener("click", () => {
+        const tempArray = Array.from(String(mainDisplay.value), Number)
+        value2 = tempArray.slice(String(value1).length + 1).join('');
+        operate(currentOperator, value1, parseInt(value2));
     },)
 
     clearButton.addEventListener("click", () => {
-        mainDisplay.value = "";
+        mainDisplay.value = '';
         value1 = '';
         value2 = '';
-    })
+    });
 }
 
 mainCalculatorFunction();
